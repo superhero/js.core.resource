b4cd68b1f8256bf7c1d854e52133b719eef92f1a
Erik Landvall <erik@landvall.se>
2018-05-14 13:29:11 +0200

    Cleaned up the bootstrap process

--- bootstrap.js
+++ bootstrap.js
@@ -4,26 +4,23 @@ config = require('./config')
 
 module.exports = (options) =>
 {
-  if('resource' in options)
-  {
-    log(`Bootstrap the resource dispatcher`)
-    const Resource = require('.')
+  log(`Bootstrap the resource dispatcher`)
+  const Resource = require('.')
 
-    for(key in options.resource)
-      switch(key)
-      {
-        case 'origin' :
-          log(`${key} set to:`, options.resource[key])
-          config[key] = options.resource[key]
-          break
+  for(key in options)
+    switch(key)
+    {
+      case 'origin' :
+        log(`${key} set to:`, options[key])
+        config[key] = options[key]
+        break
 
-        case 'contentTypeMapper' :
-          log(`${key} extended to:`, options.resource[key])
-          Object.assign(config[key], options.resource[key])
-          break
+      case 'contentTypeMapper' :
+        log(`${key} extended to:`, options[key])
+        Object.assign(config[key], options[key])
+        break
 
-        default:
-          log('Ignoring to set invalid key:', key)
-      }
-  }
+      default:
+        log('Ignoring to set invalid key:', key)
+    }
 }
